8cedd2f8f1ce1ade693fd9ac761227cc47cb04d5
Corey Ford <coyotebush22@gmail.com>
2011-07-02 12:17:43 -0700

    9.6 #2 store user id in session

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,11 +1,11 @@
 module SessionsHelper
   def sign_in(user)
-    cookies.permanent.signed[:remember_token] = [user.id, user.salt]
+    session[:user_id] = user.id
     self.current_user = user
   end
 
   def sign_out
-    cookies.delete(:remember_token)
+    session[:user_id] = nil
     self.current_user = nil
   end
 
@@ -14,7 +14,7 @@ module SessionsHelper
   end
 
   def current_user
-    @current_user ||= user_from_remember_token
+    @current_user ||= user_from_session_id
   end
 
   def signed_in?
@@ -22,11 +22,7 @@ module SessionsHelper
   end
 
 private
-  def user_from_remember_token
-    User.authenticate_with_salt(*remember_token)
-  end
-
-  def remember_token
-    cookies.signed[:remember_token] || [nil, nil]
+  def user_from_session_id
+    User.find(session[:user_id]) if session[:user_id]
   end
 end
