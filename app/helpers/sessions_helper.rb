f365bcec82aa21d4bba2ad1da75e01b4b7109ead
Corey Ford <coyotebush22@gmail.com>
2011-07-02 17:38:35 -0700

    10.2 User edit page and related security

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -21,8 +21,30 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
+  def deny_access
+    store_location
+    redirect_to signin_path, :notice => "Please sign in to access this page."
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
+
 private
   def user_from_session_id
     User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def store_location
+    session[:return_to] = request.fullpath
+  end
+
+  def clear_return_to
+    session[:return_to] = nil
   end
 end
