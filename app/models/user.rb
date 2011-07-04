9871cdb915a5cbf3a8f6c7c6816caa31be297ed9
Corey Ford <coyotebush22@gmail.com>
2011-07-04 00:02:55 -0700

    12.1 user relationships model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,13 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
 
   has_many :microposts, :dependent => :destroy
+  has_many :relationships,         :foreign_key => "follower_id",
+                                   :dependent => :destroy
+  has_many :following, :through => :relationships, :source => :followed
+  has_many :reverse_relationships, :foreign_key => "followed_id",
+                                   :class_name => "Relationship",
+                                   :dependent => :destroy
+  has_many :followers, :through => :reverse_relationships
 
   validates :name, :presence => true,
                    :length   => { :maximum => 50 }
@@ -33,6 +40,19 @@ class User < ActiveRecord::Base
     user = find_by_id(id)
     user if (user && user.salt == cookie_salt)
   end
+
+  def following?(followed)
+    relationships.find_by_followed_id(followed)
+  end
+
+  def follow!(followed)
+    relationships.create!(:followed_id => followed.id)
+  end
+
+  def unfollow!(followed)
+    relationships.find_by_followed_id(followed).destroy
+  end
+
 private
   def encrypt_password
     self.salt = make_salt if new_record?
